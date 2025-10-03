85bbfdebefdcb6af6f69709bfb2f9dba4a2bf2e0
Punsith Vidumal <punsithvidumal@gmail.com>
2025-10-03 15:24:38 +0530

    Update PaymentController.java

--- demo/src/main/java/com/example/demo/controller/PaymentController.java
+++ demo/src/main/java/com/example/demo/controller/PaymentController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controller;
 
-public class PaymentController {
-}
+import com.example.demo.model.Payment;
+import com.example.demo.model.TravelPackage;
+import com.example.demo.model.User;
+import com.example.demo.service.PaymentService;
+import com.example.demo.service.TravelPackageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import jakarta.servlet.http.HttpSession;
