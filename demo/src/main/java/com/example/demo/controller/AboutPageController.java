41a513854945cc1167f6a7bdae64b3f9f235dd5b
Punsith Vidumal <punsithvidumal@gmail.com>
2025-10-03 14:27:02 +0530

    Update AboutPageController.java

--- demo/src/main/java/com/example/demo/controller/AboutPageController.java
+++ demo/src/main/java/com/example/demo/controller/AboutPageController.java
@@ -1,4 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AboutPageController {
+
+    @GetMapping("/about-us")
+    public String showAboutUsPage() {
+        return "aboutus";  // loads /WEB-INF/views/aboutus.jsp
+    }
 }
