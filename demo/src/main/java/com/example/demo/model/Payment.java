2b9d666c3b1aa20d87b843f78010b7df46ac352d
Punsith Vidumal <punsithvidumal@gmail.com>
2025-10-03 15:22:10 +0530

    Create Payment.java

---
+++ demo/src/main/java/com/example/demo/model/Payment.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+    
+}
