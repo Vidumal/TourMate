9256527549eb45d9900ce1c6c4ec234586c8d053
Punsith Vidumal <punsithvidumal@gmail.com>
2025-10-03 14:27:22 +0530

    Update HomeController.java

--- demo/src/main/java/com/example/demo/controller/HomeController.java
+++ demo/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,4 +1,18 @@
 package com.example.demo.controller;
 
-public class HomeController {
-}
+import com.example.demo.model.Payment;
+import com.example.demo.model.TravelPackage;
+import com.example.demo.model.User;
+import com.example.demo.service.PaymentService;
+import com.example.demo.service.TravelPackageService;
+import com.example.demo.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.model.Package;
+import com.example.demo.service.PackageService;
+import org.springframework.ui.Model;
+import java.util.List;
+
+import jakarta.servlet.http.HttpServletRequest;
